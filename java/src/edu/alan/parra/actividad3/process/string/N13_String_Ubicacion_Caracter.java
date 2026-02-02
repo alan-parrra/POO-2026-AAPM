7dc8b70a7682e521aa9ec501922b108d215e3fd7
Alan parra <antonioalanmtz@gmail.com>
2026-02-01 22:52:43 -0600

    java doc

--- java/src/edu/alan/parra/actividad3/process/string/N13_String_Ubicacion_Caracter.java
+++ java/src/edu/alan/parra/actividad3/process/string/N13_String_Ubicacion_Caracter.java
@@ -1,5 +1,10 @@
 package edu.alan.parra.java.src.edu.alan.parra.actividad3.process.string;
 
+/**
+* busca la posicion de un caracter de texto y luego
+* si no lo encuentra lo devuelve
+    */
+
 public class N13_String_Ubicacion_Caracter {
 
     public int obtenerUbicacion(String texto, char caracterBuscado) {
