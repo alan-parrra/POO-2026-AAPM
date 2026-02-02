c23b6a8db9787ebb9fd8392f5e6c82cdf27b30a0
Alan parra <antonioalanmtz@gmail.com>
2026-02-01 22:56:46 -0600

    Update N3_String_Palindromo.java

--- java/src/edu/alan/parra/actividad3/process/string/N3_String_Palindromo.java
+++ java/src/edu/alan/parra/actividad3/process/string/N3_String_Palindromo.java
@@ -1,5 +1,11 @@
 package edu.alan.parra.java.src.edu.alan.parra.actividad3.process.string;
 
+/**
+* checa si una cadena de texto es palindromo y
+* determina si el texto se lee igual de izquierda a derecha y viceversa
+* dando false o true dependiendo la palabra
+    */
+
 public class N3_String_Palindromo {
 
     public boolean esPalindromo(String texto) {
